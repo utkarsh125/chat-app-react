d927a6b2520d5136881eca889393043ebb50fafd
Utkarsh Pandey <utk2665@gmail.com>
2024-02-05 21:12:34 +0530

    Frontend for Client, work on dashboard necessary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import { Cookies } from 'universal-cookie';
 import './App.css'
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 const apiKey = '2exnd5s4nxyr';
 const client = StreamChat.getInstance(apiKey);
+const authToken = false; //Will be false default as it is going to be availble if we actually logged in
+
 
 const App = () => {
+
+  if(!authToken) return <Auth />
+  
   return (
     <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
